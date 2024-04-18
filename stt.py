0a56a95173a13a65b8bbf670ca5bb1ae9b9b6ab6
Burak TAŞOVA <103398082+Landerk3@users.noreply.github.com>
2024-04-18 11:01:30 +0300

    Update stt.py

--- stt.py
+++ stt.py
@@ -52,7 +52,7 @@ def main():
     elif choice == "Analiz":
         st.write("Analiz sayfası içeriği buraya gelecek.")
     elif choice == "Maaş Tahmini":
-     
+        st.write("Analiz sayfası içeriği buraya gelecek.")
         
 
 if __name__ == "__main__":

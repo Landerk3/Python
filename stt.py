945eb8f86280a0407a960be7896f879652df2eca
Burak TAŞOVA <103398082+Landerk3@users.noreply.github.com>
2024-04-18 11:24:14 +0300

    Update stt.py

--- stt.py
+++ stt.py
@@ -84,7 +84,8 @@ def main():
         gercek = y_test
         tahmin = y_pred
         hata = gercek - tahmin
-
+        df2 = user_input_features()
+        y_pred2 = model.predict(df2)
         # Grafik oluşturma
         st.write(f"<h1 style='font-weight: bold; font-size: 29px;'>     {ModelType}</h1>", unsafe_allow_html=True)
         plt.figure(figsize=(8, 6))
@@ -97,7 +98,7 @@ def main():
         st.pyplot()
 
         # Gerekli enerji miktarını yazdırma
-        st.markdown(f"<h1 style='font-weight: bold; font-size: 24px;'>Gerekli enerji miktarı: {y_pred}</h1>",
+        st.markdown(f"<h1 style='font-weight: bold; font-size: 24px;'>Gerekli enerji miktarı: {y_pred2}</h1>",
                     unsafe_allow_html=True)
 
 if __name__ == "__main__":

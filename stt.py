8204dc0c1356e2ef1801c3cf9f5971354a9e8156
Burak TAŞOVA <103398082+Landerk3@users.noreply.github.com>
2024-04-18 13:45:45 +0300

    Update stt.py

--- stt.py
+++ stt.py
@@ -97,7 +97,7 @@ def main():
         st.pyplot()
 
         # Gerekli enerji miktarını yazdırma
-        st.markdown(f"<h1 style='font-weight: bold; font-size: 24px;'>Gerekli enerji miktarı: {y_pred2} kWh</h1>",
+        st.markdown(f"<h1 style='font-weight: bold; font-size: 24px;'>Gerekli enerji miktarı: {round(y_pred2,4)} kWh</h1>",
                     unsafe_allow_html=True)
 
 if __name__ == "__main__":
